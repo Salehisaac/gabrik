a072feeb24ebaf47e96a9e06ba4c4e04904c39c2
Salehisaac <salehisaac200281@gmail.com>
2024-02-18 15:21:03 +0330

    editing posts table 📮

--- Modules/Posts/Database/migrations/2024_02_10_210314_create_posts_table.php
+++ Modules/Posts/Database/migrations/2024_02_10_210314_create_posts_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('video');
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->enum('type', ['blog', 'project']);
             $table->softDeletes();
             $table->timestamps();
         });
